1e77bf0ddd49bd5a3ab6ee39ee8b7e6016dfa6b9
Kazuki Yasufuku <kazuki_yasufuku@dwango.co.jp>
2024-03-05 14:57:31 +0900

    fix: add cuda link and search path
    whisper.cpp needs to link cuda to build with gpu.

--- sys/build.rs
+++ sys/build.rs
@@ -52,6 +52,8 @@ fn main() {
                 println!("cargo:rustc-link-lib=culibos");
                 println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
                 println!("cargo:rustc-link-search=/opt/cuda/lib64");
+                println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+                println!("cargo:rustc-link-lib=cuda");
             }
         }
     }
